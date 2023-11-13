7efcd0fe2732364cbaab7b01475a5c0b8adca7a0
ducanh <ducanha2k46@gmail.com>
2023-11-13 20:14:35 +0700

    Change img

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -3,15 +3,15 @@ import { useNavigate } from "react-router-dom";
 
 export default function HeroSection() {
     const images = [
-        "img/gallery/img_1.jpg",
-        "img/gallery/img_2.jpg",
-        "img/gallery/img_3.jpg",
-        "img/gallery/img_4.jpg",
-        "img/gallery/img_5.jpg",
-        "img/gallery/img_6.jpg",
-        "img/gallery/img_7.jpg",
-        "img/gallery/img_8.jpg",
-        "img/gallery/img_9.jpg"
+        "img/gallery/img_1.png",
+        "img/gallery/img_2.png",
+        "img/gallery/img_3.png",
+        "img/gallery/img_4.png",
+        "img/gallery/img_5.png",
+        "img/gallery/img_6.png",
+        "img/gallery/img_7.png",
+        "img/gallery/img_8.png",
+        "img/gallery/img_9.png"
     ]
     const navigate = useNavigate();
     return (
@@ -19,7 +19,7 @@ export default function HeroSection() {
             <div className="col typography">
                 <h1 className="title">TasteBook</h1>
                 <p className="info">TasteBook không chỉ là một cuốn sách hướng dẫn nấu ăn khác—nó còn là la bàn ẩm thực của bạn. TasteBook được thiết kế để trở thành người bạn đồng hành đáng tin cậy của bạn trong nhà bếp, cung cấp các công thức nấu ăn, kỹ thuật và hiểu biết sâu sắc phục vụ cho các đầu bếp ở mọi cấp độ.</p>
-                <button className="btn" onClick={() => navigate("/login")}>explore now</button>
+                <button className="btn" onClick={() => navigate("/recipes")}>khám phá ngay</button>
             </div>
             <div className="col gallery">
                 {images.map((src, index) => (
--- src/components/ImproveSkills.js
+++ src/components/ImproveSkills.js
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "../AuthContext"
 
 export default function ImproveSkills() {
     const list = [
@@ -9,20 +10,27 @@ export default function ImproveSkills() {
         "champions",
         "champions"
     ];
+    const { isLoggedIn } = useAuth();
 
     const navigate = useNavigate();
-
+    const handleButtonClick = () => {
+        if (isLoggedIn) {
+            navigate('/profile'); // Nếu đã đăng nhập, navigate tới trang profile
+        } else {
+            navigate('/login'); // Nếu chưa đăng nhập, navigate tới trang login
+        }
+    };
     return (
         <div className="section improve-skills">
             <div className="col img">
-                <img alt="" src="/img/gallery/img_1.jpg" />
+                <img alt="" src="/img/gallery/img_improve.png" />
             </div>
             <div className="col typography">
                 <h1 className="title">Tham gia cùng chúng tôi</h1>
                 {list.map((item, index) => (
                     <p className="skill-item" key={index}>{item}</p>
                 ))}
-                <button className="btn" onClick={() => navigate("/login")}>signup now</button>
+                <button className="btn" onClick={handleButtonClick}>{isLoggedIn ? 'hồ sơ' : 'đăng nhập'}</button>
             </div>
         </div>
     )
