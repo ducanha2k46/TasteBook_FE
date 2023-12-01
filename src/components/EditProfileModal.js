fb2f94b54d8af291d568e860580846e56869cfb6
ducanh <ducanha2k46@gmail.com>
2023-12-02 00:01:04 +0700

    edit profile

--- src/components/EditProfileModal.js
+++ src/components/EditProfileModal.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useCallback } from 'react';
+import { toast } from 'react-toastify';
+const apiUrl = process.env.REACT_APP_API_URL;
 
-const EditProfileModal = ({ isOpen, onClose, profile, updateProfile }) => {
+const EditProfileModal = ({ isOpen, onClose, profile }) => {
   const [nickname, setNickname] = useState(profile.nickname);
   const convertToYYYYMMDD = (isoString) => {
     return isoString ? new Date(isoString).toISOString().split('T')[0] : '';
@@ -15,7 +17,7 @@ const EditProfileModal = ({ isOpen, onClose, profile, updateProfile }) => {
     setBirthDate(convertToYYYYMMDD(profile.birthDate));
     setEmail(profile.email);
     setBiography(profile.biography);
-  }, [profile]); 
+  }, [profile]);
 
   useEffect(() => {
     if (isOpen) {
@@ -25,37 +27,54 @@ const EditProfileModal = ({ isOpen, onClose, profile, updateProfile }) => {
 
   if (!isOpen) return null;
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const updatedProfile = { ...profile, nickname, birthDate, email, biography };
-    updateProfile(updatedProfile);
-    onClose();
+  const handleSubmit = async () => {
+    try {
+      const updateData = {
+        nickname, birthDate, biography
+      };
+      const token = localStorage.getItem('userToken');
+
+      const response = await fetch(`${apiUrl}/api/auth/update-profile`, {
+        method: 'PATCH',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(updateData)
+      });
+      const data = await response.json();
+      toast.success(data)
+    } catch (error) {
+      toast.error(error.response ? error.response.data.message : 'Lỗi không xác định');
+    }
   };
 
   return (
     <div className="modal">
       <section className="modal-content">
         <span className="close" onClick={onClose}>&times;</span>
-        <h2>Edit Profile</h2>
+        <h2>Chỉnh sửa profile</h2>
         <form onSubmit={handleSubmit}>
           <div>
-            <label>Nickname:</label>
+            <label>Biệt danh:</label>
             <input type="text" value={nickname} onChange={e => setNickname(e.target.value)} />
           </div>
           <div>
-            <label>Birth Date:</label>
+            <label>Ngày sinh:</label>
             <input type="date" value={birthDate} onChange={e => setBirthDate(e.target.value)} />
           </div>
           <div>
             <label>Email:</label>
-            <input type="email" value={email} onChange={e => setEmail(e.target.value)} />
+            <input type="email" value={email} readOnly />
           </div>
           <div>
-            <label>Biography:</label>
+            <label>Tiểu sử:</label>
             <textarea value={biography} onChange={e => setBiography(e.target.value)} />
           </div>
-          <button className='btn' type="submit">Save Changes</button>
-          <button className='btn' onClick={onClose}>Cancel</button>
+          <div className="modal-actions">
+            <button className='btn' type="submit" disabled={!nickname || !email || !birthDate}>Lưu thay đổi</button>
+            <button className='btn' type="button" onClick={onClose}>Hủy</button>
+          </div>
         </form>
       </section>
     </div>
