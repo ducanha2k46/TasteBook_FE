7f878242c8d32790af8cd4fddf097a322b9bf0dd
ducanh <ducanha2k46@gmail.com>
2023-12-02 14:54:07 +0700

    update recipe page

--- src/pages/RecipeDetail.js
+++ src/pages/RecipeDetail.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPrint, faBookmark } from '@fortawesome/free-solid-svg-icons';
+
 const apiUrl = process.env.REACT_APP_API_URL;
 
 const RecipeDetail = () => {
@@ -18,8 +21,8 @@ const RecipeDetail = () => {
     }
     const handlePrint = () => {
         window.print();
-      };
-      
+    };
+
 
     return (
         <div>
@@ -28,24 +31,30 @@ const RecipeDetail = () => {
                 <div className="recipe-image-container">
                     <img src={recipe.image} alt={recipe.name} className="recipe-image" />
                     <div className="recipe-buttons">
-                        <button type="button" className="btn">Save Recipe</button>
-                        <button type="button" className="btn" onClick={handlePrint}>Print</button>
+                        <button type="button" className="btn">
+                            <FontAwesomeIcon icon={faBookmark} />
+                            Lưu
+                        </button>
+                        <button type="button" className="btn print" onClick={handlePrint}>
+                            <FontAwesomeIcon icon={faPrint} />
+                            In 
+                        </button>
                     </div>
                 </div>
                 <div className="recipe-content">
                     <h1 className="recipe-title">{recipe.name}</h1>
                     <p className="recipe-description">{recipe.description}</p>
-                    <p className="recipe-author"><strong>Author:</strong> {recipe.author}</p>
-                    <p className="recipe-difficulty"><strong>Difficulty:</strong> {recipe.difficult}</p>
-                    <p className="recipe-preparation-time"><strong>Preparation Time:</strong> {recipe.times.Preparation}</p>
-                    <p className="recipe-cooking-time"><strong>Cooking Time:</strong> {recipe.times.Cooking}</p>
+                    <p className="recipe-author"><strong>Tác giả:</strong> {recipe.author}</p>
+                    <p className="recipe-difficulty"><strong>Độ khó:</strong> {recipe.difficult}</p>
+                    <p className="recipe-preparation-time"><strong>Thời gian chuẩn bị:</strong> {recipe.times.Preparation}</p>
+                    <p className="recipe-cooking-time"><strong>Thời gian nấu:</strong> {recipe.times.Cooking}</p>
                 </div>
             </div>
 
 
 
             <div className='recipe-ingredients'>
-                <h2>Ingredients</h2>
+                <h2>Nguyên liệu</h2>
                 <ul>
                     {recipe.ingredients.map((ingredient, index) => (
                         <li key={index}>{ingredient}</li>
@@ -54,7 +63,7 @@ const RecipeDetail = () => {
             </div>
 
             <div className='recipe-method'>
-                <h2>Method</h2>
+                <h2>Phương pháp</h2>
                 <ol>
                     {recipe.steps.map((step, index) => (
                         <li key={index}>{step}</li>
